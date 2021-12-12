d04d4c0aff16398bf8d37e6a0dd63537aa524708
SAmidSs <amid2003s@gmail.com>
2021-12-12 23:46:19 +0200

    added tour, services

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-  
+    $('.tour-tabs__top-item').on('click', function(e){
+        e.preventDefault();
+        $('.tour-tabs__top-item').removeClass('tour-tabs__top-item--active');
+        $(this).addClass('tour-tabs__top-item--active');
+
+        $('.tour-tabs__content-item').removeClass('tour-tabs__content-item--active');
+        $($(this).attr('href')).addClass('tour-tabs__content-item--active');
+    });
 })
